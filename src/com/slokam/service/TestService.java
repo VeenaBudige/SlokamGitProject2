f6ad428bcb7e227dc197172fd0e5e437fdd69909
veenabudige <veenareddy442@gmail.com>
2023-07-31 16:40:59 +0530

    git practice purpose

---
+++ src/com/slokam/service/TestService.java
@@ -0,0 +1,12 @@
+package com.slokam.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestService {
+
+	public static void main(String[] args) {
+		List<String> l=new ArrayList<String>();
+		System.out.println("Arraylist object creation");
+ 	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module SlokamDitProject2 {
+}
